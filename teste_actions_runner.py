d279ec08f19f2bf2fabb89c8e7bb836603386ef7
Sergio Barros <sbarros@viemar.com.br>
2025-12-29 15:12:06 -0300

    Adiciona linha de print para teste de commit

--- teste_actions_runner.py
+++ teste_actions_runner.py
@@ -6,6 +6,7 @@ nome_projeto = sys.argv[0]
 
 print(f"--- Iniciando teste do projeto: {nome_projeto} ---")
 print(f"Codigo anterado, teste final. Junto com Gabriel...")
+print(f"printando mais uma linha para teste de commit...")
 
 for i in range(30, 0, -1):
     print(f"Rodando... {i} segundos restantes", end="\r")
