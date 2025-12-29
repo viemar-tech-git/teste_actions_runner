18f9ca00e4f2c0e0fe25ca2c618b618cb35511da
Sergio Barros <sbarros@viemar.com.br>
2025-12-29 14:41:45 -0300

    Atualiza mensagem de impressão para incluir "Junto com Gabriel"

--- teste_actions_runner.py
+++ teste_actions_runner.py
@@ -5,7 +5,7 @@ import sys
 nome_projeto = sys.argv[0]
 
 print(f"--- Iniciando teste do projeto: {nome_projeto} ---")
-print(f"Codigo anterado, teste final.")
+print(f"Codigo anterado, teste final. Junto com Gabriel...")
 
 for i in range(30, 0, -1):
     print(f"Rodando... {i} segundos restantes", end="\r")
