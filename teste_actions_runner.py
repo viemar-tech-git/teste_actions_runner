d11a45d6ff40fd0d2c033300d3df5a71e97cd108
Sergio Barros <sbarros@viemar.com.br>
2025-12-29 14:48:15 -0300

    Corrige erro de sintaxe na impressão do nome do projeto

--- teste_actions_runner.py
+++ teste_actions_runner.py
@@ -4,7 +4,7 @@ import sys
 # Pega o nome do projeto (opcional, apenas para o print)
 nome_projeto = sys.argv[0]
 
-print(f"--- Iniciando teste do projeto: {nome_projeto} ---")
+print(f"--- Iniciando teste do projeto: {nome_projeto} ---"
 print(f"Codigo anterado, teste final. Junto com Gabriel...")
 
 for i in range(30, 0, -1):
